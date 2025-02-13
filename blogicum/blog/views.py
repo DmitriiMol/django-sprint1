7d9b3a29461d6a9fc8e5c5ad1afcc099ad95a550
Dmitrii M <dmol707@yandex.ru>
2025-02-13 18:12:13 +0300

    version 1

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -47,7 +47,7 @@ posts = [
 def index(request):
     template = 'blog/index.html'
     context = {
-        'posts': posts,
+        'posts': posts[::-1],
     }
     return render(request, template, context)
 
